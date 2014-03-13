36af0f2f67c91d490c7e403125faccd5906945a4
Vladislav Lewinn <vlewin@suse.de>
2014-03-13 16:11:21 +0100

    simpleslider-1.0.3

--- app/assets/javascripts/simpleslider.js
+++ app/assets/javascripts/simpleslider.js
@@ -1,7 +1,7 @@
-/*! Simpleslider - v0.1.0 - 2014-03-13
+/*! Simpleslider - v1.0.3 - 2014-03-13
 * https://github.com/vlewin/jquery.simpleslider
 * Copyright (c) 2014 Vladislav Lewinn; Licensed MIT */
-/*! Simplediv - v0.1.0 - 2014-02-28
+/*! Simplediv - 1.0.3 - 2014-03-13
 * https://github.com/vlewin/jquery.simplediv
 * Copyright (c) 2014 Vladislav Lewinn; Licensed MIT */
 
@@ -11,6 +11,7 @@ var SimpleSlider = function(element, opts) {
   this.init = function() {
     this.id = element.selector;
     this.element = $(this.selector);
+    this.spinner = opts.spinner;
     this.breadcrumb = opts.breadcrumb;
     this.breadcrumb_selector = opts.breadcrumb_selector;
     this.link_selector = opts.link_selector;
@@ -37,7 +38,6 @@ var SimpleSlider = function(element, opts) {
 
       var pathname = (location.pathname+location.hash);
       var childName = sessionStorage.getItem(pathname);
-
       var back_arrow = '<i class="fa fa-lg fa-arrow-circle-left"></i> ';
       var parent = '<li><a class="' + this.back_link_selector.replace('.', '') +'">' + back_arrow + parentName + '</a>';
       var child = '<li><a>' + childName + '</a></li>';
@@ -59,7 +59,12 @@ var SimpleSlider = function(element, opts) {
   };
 
   this.wait = function() {
-    this.html('<img src="/assets/spinner.gif" alt="Spinner"> Please wait ...');
+    if(this.spinner) {
+      this.html(this.spinner);
+    } else {
+      this.html('<img src="/assets/spinner.gif" alt="Spinner"> Please wait ...');
+    }
+
     return this;
   };
 
@@ -84,8 +89,10 @@ var SimpleSlider = function(element, opts) {
 
   this.forward = function(pageurl) {
     var plugin  = this;
+    plugin.wait().slide('right');
+
     $.get(pageurl, function(data) {
-      plugin.showBreadCrumb().slide('right').html(data);
+      plugin.showBreadCrumb().html(data);
     });
 
     return this;
@@ -194,3 +201,4 @@ Number.prototype.px = function () {
 Array.prototype.last = function () {
   return this[this.length - 1];
 };
+
